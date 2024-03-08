a9cd4f8047789a664b44ba2d8f3432b7b3f281db
c23037255 <HockleyB@cardiff.ac.uk>
2024-03-08 13:51:23 +0000

    added check answer function called when enter is pressed

--- wordle.js
+++ wordle.js
@@ -1,10 +1,31 @@
+word = "plane"
+
 var attempt = 1
 var letter = 1
 
 activeSquare = attempt + "-" + letter
 
+
+function checkAnswer(){
+    guess = ""
+    for (i=1;i<6;i++){
+        guess += (document.getElementById(attempt + "-" + i).innerHTML)
+    }
+    console.log(guess)
+    for (j=1;j<6;j++){
+        if (guess[j-1] == word[j-1]){
+            document.getElementById(attempt + "-" + j).style.backgroundColor = 'green'
+            document.getElementById(attempt + "-" + j).style.color = 'white'
+        } else if (guess[j-1] == word[0] || guess[j-1] == word[1] || guess[j-1] == word[2] || guess[j-1] == word[3] || guess[j-1] == word[4]){
+            document.getElementById(attempt + "-" + j).style.backgroundColor = 'orange'
+            document.getElementById(attempt + "-" + j).style.color = 'white'
+        }        
+    }
+}
+
 document.addEventListener('keydown', function(e){
     if (e.key == 'Enter'){
+        checkAnswer()
         attempt = attempt + 1
         letter = 1
         activeSquare = attempt + "-" + letter
