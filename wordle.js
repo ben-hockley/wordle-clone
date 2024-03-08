ce4d28b447726d96663be6f5d6c79af7a29b0140
c23037255 <HockleyB@cardiff.ac.uk>
2024-03-08 13:21:52 +0000

    enter sets square to next line

--- wordle.js
+++ wordle.js
@@ -1,10 +1,14 @@
-attempt = 1
-var letter = 2
+var attempt = 1
+var letter = 1
 
 activeSquare = attempt + "-" + letter
 
 document.addEventListener('keydown', function(e){
-    if (e.key == 'Backspace'){
+    if (e.key == 'Enter'){
+        attempt = attempt + 1
+        letter = 1
+        activeSquare = attempt + "-" + letter
+    } else if (e.key == 'Backspace'){
         letter = letter - 1
         activeSquare = attempt + "-" + letter
         document.getElementById(activeSquare).innerHTML = ""
