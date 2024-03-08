8360a613f47649217c24c3cb7c4ff9b54d51d70c
c23037255 <HockleyB@cardiff.ac.uk>
2024-03-08 14:46:41 +0000

    checks for 5 letter answer and checks for correct answer

--- wordle.js
+++ wordle.js
@@ -11,6 +11,14 @@ function checkAnswer(){
     for (i=1;i<6;i++){
         guess += (document.getElementById(attempt + "-" + i).innerHTML)
     }
+    if (word == guess){
+        alert("You win in " + attempt + " attempts!")
+        for (k=1;k<6;k++){
+            document.getElementById(attempt + "-" + k).style.backgroundColor = 'green'
+            document.getElementById(attempt + "-" + k).style.color = 'white'
+        }
+    }
+    else if (guess.length == 5){
     console.log(guess)
     for (j=1;j<6;j++){
         if (guess[j-1] == word[j-1]){
@@ -19,16 +27,20 @@ function checkAnswer(){
         } else if (guess[j-1] == word[0] || guess[j-1] == word[1] || guess[j-1] == word[2] || guess[j-1] == word[3] || guess[j-1] == word[4]){
             document.getElementById(attempt + "-" + j).style.backgroundColor = 'orange'
             document.getElementById(attempt + "-" + j).style.color = 'white'
-        }        
+        }  
+    }
+    attempt = attempt + 1
+    letter = 1
+    activeSquare = attempt + "-" + letter
+    } else {
+        alert("must enter a 5 letter word")
     }
 }
+
 
 document.addEventListener('keydown', function(e){
     if (e.key == 'Enter'){
         checkAnswer()
-        attempt = attempt + 1
-        letter = 1
-        activeSquare = attempt + "-" + letter
     } else if (e.key == 'Backspace'){
         letter = letter - 1
         activeSquare = attempt + "-" + letter
